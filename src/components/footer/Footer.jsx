27d0001b3cdf3ede94ec2890d43a94d1af8b3fb4
Kelly Vasquez Z <kefervaza.19@gmail.com>
2022-10-29 11:26:54 -0500

    Footer responsive

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -6,48 +6,69 @@ export const Footer = () => {
   return (
     <footer className={ styles.footerArea }>
         <div className="d-flex justify-content-evenly p-30">
-            <div>
-                <a href="#">
+            <div className="d-flex flex-column align-items-center">
+                <a href="https://www.audifarma.com.co/"  target="_blank">
                     <img src={ logo } alt="" className={ styles.footerLogo } />
                 </a>
+                <small>Copyright © 2022 Audifarma University</small>
             </div>
+            <hr className="w-100" />
             <div className={ styles.footerMenu }>
-                <h3>Sobre nosotros</h3>
+                <div className="d-flex justify-content-between w-100">
+                    <h3>Sobre nosotros</h3>
+                    <i className="fa-solid fa-chevron-down"></i>
+                </div>
                 <ul>
-                    <li><a href="#">Quienes somos</a></li>
-                    <li><a href="#">Misión</a></li>
-                    <li><a href="#">Visión</a></li>
-                    <li><a href="#">Compañía</a></li>
-                    <li><a href="#">Programas</a></li>
+                    <li><a href="/">Quienes somos</a></li>
+                    <li><a href="/">Misión</a></li>
+                    <li><a href="/">Visión</a></li>
+                    <li><a href="/">Compañía</a></li>
+                    <li><a href="/">Programas</a></li>
                 </ul>
             </div>
+            <hr className="w-100" />
             <div className={ styles.footerMenu }>
-                <h3>Normatividad</h3>
+                <div className="d-flex justify-content-between w-100">
+                    <h3>Ayuda</h3>  
+                    <i className="fa-solid fa-chevron-down"></i>  
+                </div>
                 <ul>
-                    <li><a href="#">Términos y condiciones</a></li>
-                    <li><a href="#">Código de ética</a></li>
-                    <li><a href="#">Política de privacidad</a></li>
-                    <li><a href="#">Resolución 521 de 2020</a></li>
+                    <li><a href="/">Términos y condiciones</a></li>
+                    <li><a href="/">Código de ética</a></li>
+                    <li><a href="/">Política de privacidad</a></li>
+                    <li><a href="/">Resolución 521 de 2020</a></li>
+                    <li><a href="/">Preguntas frecuentes</a></li>
                 </ul>
             </div>
-        </div>
-        <hr />
-        <div className="d-flex flex-column justify-content-center align-items-center">
-            <div className="d-flex gap-10">
-                <a href="#">
-                    <i className="fa-brands fa-facebook-f"></i>
-                </a>
-                <a href="#">
-                    <i className="fa-brands fa-instagram"></i>
-                </a>
-                <a href="#">
-                    <i className="fa-brands fa-twitter"></i>
-                </a>
-                <a href="#">
-                    <i className="fa-brands fa-linkedin-in"></i>
-                </a>
+            <hr className="w-100" />
+            <div className={`${ styles.footerMenu } ${ styles.socialMedia }`}>
+                <div className="d-flex justify-content-between w-100">
+                    <h3>Siguenos</h3>
+                    <i className="fa-solid fa-chevron-down"></i>
+                </div>
+                <ul className="d-flex gap-10">
+                    <li>
+                        <a href="https://es-la.facebook.com/Audifarma" target="_blank">
+                            <i className="fa-brands fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/audifarma_sa/" target="_blank">
+                            <i className="fa-brands fa-instagram"></i>
+                        </a> 
+                    </li>
+                    <li>
+                        <a href="https://twitter.com/Audifarma_SA" target="_blank">
+                            <i className="fa-brands fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.linkedin.com/company/audifarma-s.a/" target="_blank">
+                            <i className="fa-brands fa-linkedin-in"></i>
+                        </a>
+                    </li>
+                </ul>
             </div>
-            <small>Copyright © 2022 Audifarma University</small>
         </div>
     </footer>
   )
