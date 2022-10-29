e97743ae4b88ce42344a5db0d70db696d04ed52d
Kelly Vasquez Z <kefervaza.19@gmail.com>
2022-10-29 12:44:39 -0500

    Navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,42 +1,64 @@
+import { useState } from 'react'
 import styles from "./navbar.module.css";
 import logo from "../../assets/img/audifarma-university-logo.png";
 import logoWhite from "../../assets/img/audifarma-university-logo-white.png";
 
 export const Navbar = ({ elevationNavbar }) => {
+
+  const [active, setActive] = useState('');
+
+  const menuActive = () => {
+    if(active) {
+      setActive('')
+      return
+    }
+    setActive('active')
+  };
+
   return (
     <header className={ styles.headerArea }>
       <nav className={`${styles.navbar} ${ elevationNavbar ? styles.elevationNavbar : '' }`}>
-        <div className="container">
-          <a href="#">
+        <div>
+          <a href="/">
             <img src={ elevationNavbar ? logo : logoWhite } alt="" className={ styles.navbarLogo } />
           </a>
         </div>
-        <div className={ styles.menu }>
+        <div className={ `${ styles.menu } ${ styles[active] ?? '' }` }>
           <ul className={ styles.dropdownMenu }>
             <li>
-              <a href="#"> <i className="fa-solid fa-house"></i> Inicio </a>
+              <a href="/"> <i className="fa-solid fa-house"></i> Inicio </a>
             </li>
             <li>
-              <a href="#"> Quienes Somos </a>
+              <a> Quienes Somos </a>
               <ul>
                 <li>
-                  <a href="#">Hello</a>
+                  <a href="/">Compañía</a>
                 </li>
                 <li>
-                  <a href="#">Hello</a>
+                  <a href="/">Misión</a>
                 </li>
                 <li>
-                  <a href="#">Hello</a>
+                  <a href="/">Visión</a>
                 </li>
               </ul>
             </li>
             <li>
-              <a href="#"> Programas </a>
+              <a href="/"> Programas </a>
             </li>
             <li>
-              <a href="#"> Contáctenos </a>
+              <a href="/"> Contáctenos </a>
             </li>
           </ul>
+          <div className={ styles.logoMobile}>
+            <a href="/">
+              <img src={ logo } alt="" className={ styles.navbarLogo } />
+            </a>
+          </div>
+        </div>
+        <div className={ `${styles.toggleMenu} ${ styles[active] ?? '' }` } onClick={ menuActive }>
+          <div></div>
+          <div></div>
+          <div></div>
         </div>
       </nav>
     </header>
